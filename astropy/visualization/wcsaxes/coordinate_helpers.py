65ea4c62ce2b0aae92a063d775811149ac7cb5be
Adrien Thob <adrien.thob@gmail.com>
2025-10-24 15:56:22 -0400

    Updating user warning's stacklevel to 2

--- astropy/visualization/wcsaxes/coordinate_helpers.py
+++ astropy/visualization/wcsaxes/coordinate_helpers.py
@@ -758,6 +758,7 @@ class CoordinateHelper:
                 "overwritten during the rendering. Use the 'loc' keyword "
                 "argument instead.",
                 AstropyUserWarning,
+                stacklevel=2,
             )
 
         self._axislabel_set = True
